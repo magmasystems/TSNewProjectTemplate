4e98ac174869380ef92d045709642bb9528a2c12
magmasystems <magmasystems@gmail.com>
2020-12-11 14:33:08 -0500

    Updated startServices script

--- scripts/startServices.sh
+++ scripts/startServices.sh
@@ -4,4 +4,4 @@ export NODE_ENV=local
 
 # Stop all servers and start the server as a daemon
 forever stopall
-forever start /home/ubuntu/TSNewProjectTemplate/dist/src/app.js
+forever start -c node /home/ubuntu/TSNewProjectTemplate/dist/src/app.js mock
