dff6b1737b32c349362bb4e66ee2c5603fbb0077
magmasystems <magmasystems@gmail.com>
2020-12-11 14:47:32 -0500

    Script enhancements

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-mkdir /home/ubuntu/TSNewProjectTemplate
+if [ ! -d "/home/ubuntu/TSNewProjectTemplate" ] 
+then
+    echo "Directory /home/ubuntu/TSNewProjectTemplate DOES NOT exists." 
+    mkdir /home/ubuntu/TSNewProjectTemplate
+fi
+
 cd /home/ubuntu/TSNewProjectTemplate
 
 sudo npm install
